e102d265eefef909685a868b860548a431260c69
ccdkk <cdk243@gmail.com>
2023-01-18 14:56:33 +0900

    say hello

--- GithubActionTest/GithubActionTest/ViewController.swift
+++ GithubActionTest/GithubActionTest/ViewController.swift
@@ -13,6 +13,7 @@ class ViewController: UIViewController {
         super.viewDidLoad()
         print("hleeo")
         print("hleeo")
+        print("hleeo")
         // Do any additional setup after loading the view.
     }
 
